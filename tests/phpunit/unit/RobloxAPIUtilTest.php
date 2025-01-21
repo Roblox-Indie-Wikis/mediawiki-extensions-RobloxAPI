a5324ba04189e04930ab22345d6e319263504aab
abaddriverlol <thisisnotmyname275@gmail.com>
2025-01-21 18:35:51 +0100

    style: fix lint errors

--- tests/phpunit/unit/RobloxAPIUtilTest.php
+++ tests/phpunit/unit/RobloxAPIUtilTest.php
@@ -160,10 +160,10 @@ class RobloxAPIUtilTest extends \MediaWikiUnitTestCase {
 	public function testVerifyIsRobloxCdnUrl(): void {
 		$urlUtils = new UrlUtils();
 
-		self::assertTrue( RobloxAPIUtil::verifyIsRobloxCdnUrl( 'https://tr.rbxcdn.com/30DAY-Avatar-7B1E1A9240F5DE0598D6FD97DBC8859F-Png/140/140/Avatar/Png/noFilter.png',
-			$urlUtils ) );
-		self::assertTrue( RobloxAPIUtil::verifyIsRobloxCdnUrl( 'https://tr.rbxcdn.com/30DAY-Avatar-7B1E1A9240F5DE0598D6FD97DBC8859F-Png/140/140/Avatar/Png/noFilter.webp',
-			$urlUtils ) );
+		self::assertTrue( RobloxAPIUtil::verifyIsRobloxCdnUrl( 'https://tr.rbxcd' .
+			'n.com/30DAY-Avatar-7B1E1A9240F5DE0598D6FD97DBC8859F-Png/140/140/Avatar/Png/noFilter.png', $urlUtils ) );
+		self::assertTrue( RobloxAPIUtil::verifyIsRobloxCdnUrl( 'https://tr.rbxcdn.co' .
+			'm/30DAY-Avatar-7B1E1A9240F5DE0598D6FD97DBC8859F-Png/140/140/Avatar/Png/noFilter.webp', $urlUtils ) );
 
 		self::assertFalse( RobloxAPIUtil::verifyIsRobloxCdnUrl( 'https://roblox.com/1234/', $urlUtils ) );
 		self::assertFalse( RobloxAPIUtil::verifyIsRobloxCdnUrl( 'https://t0.rbxcdn.com///https://google.com/',
