e5a5c2ac21018dea154ab0646a20905ab6cd92a9
SomeRandomDeveloper <git@srd.dev>
2026-08-07 03:58:20 +0200

    refactor: support Parser implementing MessageLocalizer on 1.47+

--- src/Util/RobloxAPIUtils.php
+++ src/Util/RobloxAPIUtils.php
@@ -11,6 +11,7 @@ use LogicException;
 use MediaWiki\Config\ServiceOptions;
 use MediaWiki\Html\Html;
 use MediaWiki\Json\FormatJson;
+use MediaWiki\Language\MessageLocalizer;
 use MediaWiki\Parser\Parser;
 use MediaWiki\Utils\UrlUtils;
 use StatusValue;
@@ -111,19 +112,25 @@ class RobloxAPIUtils {
 	/**
 	 * @return string Wikitext
 	 */
-	public function formatStatusValue( StatusValue $status, Parser $parser ): string {
+	public function formatStatusValue( StatusValue $status, MessageLocalizer|Parser $localizer ): string {
 		if ( $status->isGood() ) {
 			throw new LogicException( __METHOD__ . ' should only be called for bad StatusValues!' );
+		}
+
+		if ( $localizer instanceof Parser ) {
+			// Parser::msg doesn't implement MessageLocalizer and only supports strings and not MessageSpecifiers...
+			// TODO This is be fixed on 1.47+
+			$localizerCallback = static fn ( $msg ) => wfMessage( $msg )
+				->inLanguage( $localizer->getTargetLanguage() )
+				->page( $localizer->getPage() );
+		} else {
+			$localizerCallback = $localizer->msg( ... );
 		}
 
 		$result = '';
 
 		foreach ( $status->getMessages() as $msg ) {
-			// Parser::msg doesn't allow a MessageSpecifier as the first arg...
-			$message = wfMessage( $msg )
-				->inLanguage( $parser->getTargetLanguage() )
-				->page( $parser->getPage() )
-				->plain();
+			$message = $localizerCallback( $msg )->plain();
 
 			$result .= $this->options->get( RobloxAPIConstants::ConfShowPlainErrors )
 				? $message
